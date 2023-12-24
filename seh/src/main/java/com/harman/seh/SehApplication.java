105267db00fb50c27b76421d1566a0ea22e84c2d
saichethan111 <saichethan529@gmail.com>
2023-12-24 17:10:14 +0530

    Initial code stub

---
+++ seh/src/main/java/com/harman/seh/SehApplication.java
@@ -0,0 +1,13 @@
+package com.harman.seh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SehApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SehApplication.class, args);
+	}
+
+}
---
+++ seh/src/test/java/com/harman/seh/SehApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harman.seh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SehApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
